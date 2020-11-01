7889879165f9d0b58fd134fdd620b9792e465950
JoseEduardoSalles <jose.salles@bandtec.com.br>
2020-11-01 13:34:42 -0300

    ajustes página

--- Site/script.js
+++ Site/script.js
@@ -2,8 +2,20 @@ function entrar() {
     var email = in_login.value;
     var senha = in_senha.value;
 
-    if (email == "admin@gmail.com" && senha == "1234") {
+    if (email == '' || senha == '') {
+        alert('Insira um campo válido')
+    }
+    else if (email == "admin@gmail.com" && senha == "1234") {
         window.location.assign("../Site/home.html") //chamando a window e dps ta pegando a location da janela
         // e o assing pra eu ir pra alguma pagina 
+    }
+    else if (email == 'lucas@gmail.com' && senha == 'batata') {
+        window.location.assign("../Site/home.html")
+    }
+    else if (email == 'ProfBrandao@gmail.com' && senha == 'bandtec') {
+        window.location.assign("../Site/home.html")
+    }
+    else {
+        alert('Insira um usuário válido')
     }
 }
